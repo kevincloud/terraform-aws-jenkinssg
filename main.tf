16f53a418b446cd828a3b51e2a12d265b0a447bd
Kevin Cochran <kcochran@hashicorp.com>
2020-01-10 15:47:22 -0500

    open port 9000

--- main.tf
+++ main.tf
@@ -31,6 +31,13 @@ resource "aws_security_group" "jenkins-server-sg" {
        cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+       from_port = 9000
+       to_port = 9000
+       protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port = 0
         to_port = 0
